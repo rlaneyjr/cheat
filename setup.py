28e993f72eb1ef0587b2411aca2bfae6dbccb7e4
Ricky Laney <rlaneyjr@gmail.com>
2018-07-26 08:59:56 -0400

    Auto Push

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author       = 'Ricky Laney',
     author_email = 'rlaneyjr@gmail.com',
     license      = 'GPL3',
-    description  = 'This is my version of cheat completely stolen from Chris Allen Lane'
+    description  = 'This is my version of cheat completely stolen from Chris Allen Lane '
     'cheat allows you to create and view interactive cheatsheets '
     'on the command-line. It was designed to help remind *nix system '
     'administrators of options for commands that they use frequently, but not '
